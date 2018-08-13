9ea121b61dfd5a3bc3dff0b5c83e549f086e64d0
november <nnwilliams8@gmail.com>
2018-08-13 16:09:43 -0400

    in progress

--- api/user.js
+++ api/user.js
@@ -44,21 +44,6 @@ router.get('/:id/suggestedmatches', async (req, res, next) => {
   }
 })
 
-router.post('/login', (req, res, next) => {
-  User.findOne({ where: { email: req.body.email } })
-    .then(user => {
-      if (!user) {
-        res.status(401).send('User not found')
-      } else if (!user.correctPassword(req.body.password)) {
-        res.status(401).send('Incorrect password')
-      } else {
-        res.json(user)
-        //req.login(user, err => (err ? next(err) : res.json(user)))
-      }
-    })
-    .catch(next)
-})
-
 router.post('/signup', (req, res, next) => {
   User.create(req.body)
     .then(user => {
