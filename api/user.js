8a7d12305bf8ea0f6836cd6c1d0f62bc9908154d
november <nnwilliams8@gmail.com>
2018-08-13 15:54:42 -0400

    Refactor(App): removed messy components causing bugs

--- api/user.js
+++ api/user.js
@@ -17,21 +17,25 @@ router.get('/:id', async (req, res, next) => {
 router.get('/:id/suggestedmatches', async (req, res, next) => {
   try {
     const ourUserId = Number(req.params.id)
+    const match = await SuggestedMatchesPerUser.findOne({
+      where: { userId: ourUserId },
+    })
 
-    const match = await SuggestedMatchesPerUser.findOne({ where: { userId: ourUserId } })
-
+    if (!match) res.send('user not matched!').status(501)
     const matchId = match.suggestedMatchId
 
     const possibleUserMatchIds = await SuggestedMatchesPerUser.findAll({
-      where: { suggestedMatchId: matchId}
+      where: { suggestedMatchId: matchId },
     })
 
-    let possibleMatches = [];
+    let possibleMatches = []
     for (var i = 0; i < possibleUserMatchIds.length; i++) {
       if (possibleUserMatchIds[i].userId === ourUserId) {
         possibleUserMatchIds.splice(i, 1)
       }
-      possibleMatches.push(await User.findOne({where: { id: possibleUserMatchIds[i].userId}}))
+      possibleMatches.push(
+        await User.findOne({ where: { id: possibleUserMatchIds[i].userId } })
+      )
     }
 
     res.json(possibleMatches).status(200)
@@ -81,4 +85,3 @@ router.post('/signup', (req, res, next) => {
 //     })
 //     .catch(next);
 // });
-
