5949fe5021c0a400f66ab8747106fdebe264c2ba
november <nnwilliams8@gmail.com>
2018-08-14 11:40:54 -0400

    suggestedmatches handled

--- api/user.js
+++ api/user.js
@@ -10,7 +10,7 @@ router.get('/:id', async (req, res, next) => {
       const user = await User.findById(req.params.id)
       res.json(user).status(200)
     } else {
-      res.send('mind yer own business').status(403)
+      res.send('FORBIDDEN').status(403)
     }
   } catch (err) {
     next(err)
@@ -21,32 +21,35 @@ router.get('/:id', async (req, res, next) => {
 router.get('/:id/suggestedmatches', async (req, res, next) => {
   try {
     const ourUserId = Number(req.params.id)
-    console.log('req.user object in api/user', req.user)
     if (req.user.id === ourUserId) {
       const match = await SuggestedMatchesPerUser.findOne({
         where: { userId: ourUserId },
       })
+      if (match) {
+        const matchId = match.suggestedMatchId
 
-      if (!match) res.send('user not matched!').status(501)
-      const matchId = match.suggestedMatchId
+        const possibleUserMatchIds = await SuggestedMatchesPerUser.findAll({
+          where: { suggestedMatchId: matchId },
+        })
 
-      const possibleUserMatchIds = await SuggestedMatchesPerUser.findAll({
-        where: { suggestedMatchId: matchId },
-      })
-
-      let possibleMatches = []
-      for (var i = 0; i < possibleUserMatchIds.length; i++) {
-        if (possibleUserMatchIds[i].userId === ourUserId) {
-          possibleUserMatchIds.splice(i, 1)
+        let possibleMatches = []
+        for (var i = 0; i < possibleUserMatchIds.length; i++) {
+          if (possibleUserMatchIds[i].userId === ourUserId) {
+            possibleUserMatchIds.splice(i, 1)
+          }
+          possibleMatches.push(
+            await User.findOne({
+              where: { id: possibleUserMatchIds[i].userId },
+            })
+          )
         }
-        possibleMatches.push(
-          await User.findOne({ where: { id: possibleUserMatchIds[i].userId } })
-        )
-      }
 
-      res.json(possibleMatches).status(200)
+        res.json(possibleMatches).status(200)
+      } else {
+        res.send('No matches found')
+      }
     } else {
-      res.send('Mind your own business!').status(403)
+      res.send('FORBIDDEN').status(403)
     }
   } catch (e) {
     next(e)
