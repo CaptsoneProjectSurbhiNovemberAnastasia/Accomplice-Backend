5141e8e5918b0dea24929802ceed9160a357966e
november <nnwilliams8@gmail.com>
2018-08-16 14:44:21 -0400

    Feature(Activities): Users can now select activities

--- api/activity.js
+++ api/activity.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { User, Activity } = require('../db/models')
+const { User, Activity, Tag } = require('../db/models')
 
 module.exports = router
 
@@ -16,9 +16,22 @@ router.get('/', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
   try {
     const you = await User.findById(req.user.id)
-    const yourNewActivity = await Activity.create(req.body)
+
+    const yourActivityTags = []
+
+    for (let i = 0; i < req.body.tags.length; i++) {
+      const tag = await Tag.findById(req.body.tags[i].id)
+      yourActivityTags.push(tag)
+    }
+
+    const yourNewActivity = await Activity.create({ name: req.body.name })
+
     await you.setActivity(yourNewActivity)
+
+    await yourNewActivity.setTags(yourActivityTags)
+
     const yourActivity = await you.getActivity()
+
     res.json(yourActivity)
   } catch (e) {
     next(e)
--- api/zipcode.js
+++ api/zipcode.js
@@ -1,12 +1,8 @@
 const router = require('express').Router()
 const axios = require('axios')
-const { GOOGLE_GEO_API_KEY } = '../secrets'
-
 module.exports = router
 
 router.get('/', (req, res, next) => {
-  console.log('Inside zipcode')
-  console.log('Google Key', process.env.GOOGLE_GEO_API_KEY)
   /*
   route format:
     `.../api/zipcode/?lat=${lat}&lng=${lng}`
@@ -15,7 +11,9 @@ router.get('/', (req, res, next) => {
   const lng = req.query['lng']
   axios
     .get(
-      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${GOOGLE_GEO_API_KEY}`
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${
+        process.env.GOOGLE_GEO_API_KEY
+      }`
     )
     .then(address => {
       let last = address.data.results[0].address_components.length - 1
@@ -24,5 +22,5 @@ router.get('/', (req, res, next) => {
 
       res.json(zipcode)
     })
-    .catch(next)
+    .catch(e => next(e))
 })
