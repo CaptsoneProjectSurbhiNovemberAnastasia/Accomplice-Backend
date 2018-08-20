9ce8dce57a60619ebf3f2e3fd9a7f4c3268a4c9b
november <nnwilliams8@gmail.com>
2018-08-19 20:09:59 -0400

    integrated changes

--- api/user.js
+++ api/user.js
@@ -25,62 +25,58 @@ router.put('/:id', async (req, res, next) => {
 router.get('/:id/suggestedmatches', async (req, res, next) => {
   try {
     const ourUserId = Number(req.params.id)
-    if (req.user.id === ourUserId) {
-      const matches = await SuggestedMatchesPerUser.findAll({
-        where: { userId: ourUserId },
-      })
-      if (matches) {
-        const matchIds = matches.map(a => a.suggestedMatchId)
+    const matches = await SuggestedMatchesPerUser.findAll({
+      where: { userId: ourUserId },
+    })
+    if (matches) {
+      const matchIds = matches.map(a => a.suggestedMatchId)
 
-        let possibleUserMatchIds = []
+      let possibleUserMatchIds = []
 
-        // instead of Op.in we have to use a for loop...
-        for (let i = 0; i < matchIds.length; i++) {
-          let currentMatchIds = await SuggestedMatchesPerUser.findAll({
-            where: { suggestedMatchId: matchIds[i] },
-          })
-          possibleUserMatchIds = possibleUserMatchIds.concat(currentMatchIds)
-        }
-
-        //filter out ourself
-        possibleUserMatchIds = possibleUserMatchIds.filter(
-          mch => mch.userId !== ourUserId
-        )
-
-        let possibleMatches = []
-
-        // again, Op.in doesn't work for some reason, so a for loop
-        for (let i = 0; i < possibleUserMatchIds.length; i++) {
-          possibleMatches.push(
-            await User.findOne({
-              where: { id: possibleUserMatchIds[i].userId },
-            })
-          )
-        }
-
-        // filter out duplicates
-        let keys = possibleMatches.map(ele => ele.id)
-        possibleMatches = possibleMatches.filter(
-          (ele, i) => keys.indexOf(ele.id) === i
-        )
-
-        // now we need each user's activity and its tags
-        for (let i = 0; i < possibleMatches.length; i++) {
-          if (possibleMatches[i].activityId) {
-            const theirActivity = await possibleMatches[i].getActivity()
-            const theirActivityTags = await theirActivity.getTags()
-            // just put the activity and tags on there so we can look at it on the front end
-            theirActivity.dataValues.tags = theirActivityTags
-            possibleMatches[i].dataValues.activity = theirActivity
-          }
-        }
-
-        res.json(possibleMatches).status(200)
-      } else {
-        res.send('No matches found')
+      // instead of Op.in we have to use a for loop...
+      for (let i = 0; i < matchIds.length; i++) {
+        let currentMatchIds = await SuggestedMatchesPerUser.findAll({
+          where: { suggestedMatchId: matchIds[i] },
+        })
+        possibleUserMatchIds = possibleUserMatchIds.concat(currentMatchIds)
       }
+
+      //filter out ourself
+      possibleUserMatchIds = possibleUserMatchIds.filter(
+        mch => mch.userId !== ourUserId
+      )
+
+      let possibleMatches = []
+
+      // again, Op.in doesn't work for some reason, so a for loop
+      for (let i = 0; i < possibleUserMatchIds.length; i++) {
+        possibleMatches.push(
+          await User.findOne({
+            where: { id: possibleUserMatchIds[i].userId },
+          })
+        )
+      }
+
+      // filter out duplicates
+      let keys = possibleMatches.map(ele => ele.id)
+      possibleMatches = possibleMatches.filter(
+        (ele, i) => keys.indexOf(ele.id) === i
+      )
+
+      // now we need each user's activity and its tags
+      for (let i = 0; i < possibleMatches.length; i++) {
+        if (possibleMatches[i].activityId) {
+          const theirActivity = await possibleMatches[i].getActivity()
+          const theirActivityTags = await theirActivity.getTags()
+          // just put the activity and tags on there so we can look at it on the front end
+          theirActivity.dataValues.tags = theirActivityTags
+          possibleMatches[i].dataValues.activity = theirActivity
+        }
+      }
+
+      res.json(possibleMatches).status(200)
     } else {
-      res.send('FORBIDDEN').status(403)
+      res.send('No matches found')
     }
   } catch (e) {
     next(e)
