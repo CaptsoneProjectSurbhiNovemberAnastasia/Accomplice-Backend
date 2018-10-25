bbefdd94d1d3481412786e2b157fd617039f01c5
Surbhi Gupta <surabhi.gupta.sai@gmail.com>
2018-10-25 12:41:21 -0400

    database validation removed

--- db/db.js
+++ db/db.js
@@ -7,7 +7,7 @@ console.log('Database name is', databaseName)
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
-    logging: true,
+    logging: false,
   }
 )
 module.exports = db
